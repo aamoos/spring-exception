8398388c8928d2f68ac07405e9bab55dca69a315
kimjaesung <aamoos@naver.com>
2024-12-04 10:44:47 +0900

    HandlerExceptionResolver 시작

---
+++ src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,26 @@
+package hello.exception;
+
+import hello.exception.resolver.MyHandlerExceptionResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**");
+    }
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+    }
+}
